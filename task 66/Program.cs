3bf3db5783499b715d87b0a48cbbd7d070823623
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-28 09:15:45 +0700

    66

---
+++ task 66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число начало: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число конец: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел равна {Summa(n)}");
+int Summa(int n)
+{
+    if (n == 0) return 0;
+    else return n + Summa(n - 1);
+}
