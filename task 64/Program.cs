9bea5b103876f91a0bd92ee37a2ce8ec7673dc96
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-28 09:13:15 +0700

    64

---
+++ task 64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите число начало: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число конец: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Number(n, m));
+
+int Number(int n, int m)
+{
+    if (n == m)
+        return n;
+    else if (n < m)
+        Console.Write($"{Number(n, m - 1)}, ");
+    else
+        Console.Write($"{Number(n, m + 1)}, ");
+    return m;
+}
